3a69a66ac12ea8d9389bbc3a495ab81e8068101e
“ <2528743003@qq.com>
2020-07-22 21:42:20 +0800

    更新Java基础07

---
+++ java基础/day07-数组练习（常见算法）/我的代码/src/com/yuanbaoqiang/array/HuiXingShu.java
@@ -0,0 +1,13 @@
+package com.yuanbaoqiang.array;
+
+import java.util.Scanner;
+
+public class HuiXingShu {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入一个数字：");
+        int len = scanner.nextInt();
+
+
+    }
+}
